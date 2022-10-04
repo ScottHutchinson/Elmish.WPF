d28dd41ef8944897592ae13aca584234a0bfec28
Tyson Williams <tyson.d.w@gmail.com>
2022-10-04 18:40:11 -0500

    No exception for me involving Microsoft.Xaml.Behaviors

--- src/Samples/SubModelSeq/MainWindow.xaml.cs
+++ src/Samples/SubModelSeq/MainWindow.xaml.cs
@@ -3,13 +3,6 @@
 namespace Elmish.WPF.Samples.SubModelSeq {
   public partial class MainWindow : Window {
     public MainWindow() {
-      /* This next line is needed to prevent a runtime exception
-      * 'Could not load file or assembly 'Microsoft.Xaml.Behaviors...'
-      * See https://github.com/microsoft/XamlBehaviorsWpf/issues/86
-      */
-      _ = new Microsoft.Xaml.Behaviors.EventTrigger {
-        SourceName = "foo"
-      };
       InitializeComponent();
     }
   }
